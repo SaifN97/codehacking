0557a5d80fac28879fe18ba54ac9bf7790d1e53b
SaifN97 <saifnarpali@gmail.com>
2020-06-29 22:22:50 +0530

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,21 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+
+Route::resource('admin/users', 'AdminUsersController');
+
+
+Route::get('/admin', function () {
+    return view('admin.index');
+});
