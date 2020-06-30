744fb022768a7b39fcd958ad43aa0c4bda2f845a
SaifN97 <saifnarpali@gmail.com>
2020-06-30 20:14:46 +0530

    users page working without photos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,7 +25,9 @@ Route::get('/home', 'HomeController@index');
 
 Route::resource('admin/users', 'AdminUsersController');
 
+Route::group(['middleware' => 'web'], function () {
 
-Route::get('/admin', function () {
-    return view('admin.index');
+    Route::get('/admin', function () {
+        return view('admin.index');
+    });
 });
