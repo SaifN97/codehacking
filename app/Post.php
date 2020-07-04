7aad1a6baba3e507b0c14ed94dd474f2b4e762f6
SaifN97 <saifnarpali@gmail.com>
2020-07-04 23:06:30 +0530

    able to create posts with dynamic categories

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'categoty_id',
+        'category_id',
         'photo_id',
         'user_id',
         'title',
